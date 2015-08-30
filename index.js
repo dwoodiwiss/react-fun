c435a1588f6379e1f0d4c46f4a650489302936d9
Darren Woodiwiss <darrenwoodiwiss@gmail.com>
2015-08-30 15:59:43 +0100

    get users tweets and save them to a json file

--- index.js
+++ index.js
@@ -1,4 +1,26 @@
 var Twitter       = require('twitter');
 var Config        = require('./config.js');
+var fs            = require('fs');
 
-console.log(Config.twitter);
+var client = new Twitter({
+  consumer_key: Config.twitter.consumer_key,
+  consumer_secret: Config.twitter.consumer_secret,
+  access_token_key: Config.twitter.access_token_key,
+  access_token_secret: Config.twitter.access_token_secret
+});
+
+var params = {screen_name: 'DWoodiwiss'};
+client.get('statuses/user_timeline', params, function(error, tweets, response){
+  if (!error) {
+    writeFile(params.screen_name.toLowerCase(), JSON.stringify(tweets));
+  }
+});
+
+function writeFile(user, tweets) {
+  fs.writeFile("data/" + user + ".json", tweets, function(err) {
+    if(err) {
+      return console.log(err);
+    }
+    console.log(user + " file was saved!");
+  });
+}
